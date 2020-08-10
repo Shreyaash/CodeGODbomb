f792dcaf49e53ef2c3b11bdad19cba74c0554fc4
rdxbombauto <rdxbombauto@gmail.com>
2020-08-10 14:39:56 +0530

    first

--- home/admin.py
+++ home/admin.py
@@ -3,10 +3,6 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Attackinfo
-from .models import Userinfo
-from .models import Workerinfo
 
 
 admin.site.register(Attackinfo)
-admin.site.register(Userinfo)
-admin.site.register(Workerinfo)
